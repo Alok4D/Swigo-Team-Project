dfde0a656b0b3938ade51fe62d22c1e1ab9a0df3
Alok4D <alokroy602701@gmail.com>
2024-08-25 23:10:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+function App() {
+ 
+
+  return (
+    <>
+    
+      <h1 className='btn btn-primary text-zinc-50'>Vite + React</h1>
+ 
+  
+    </>
+  )
+}
+
+export default App
